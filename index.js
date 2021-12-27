20a2f2028bcc8cc3b117efb92091c81eca454fa2
jonathan bola <bolajonathan@yahoo.com>
2021-12-27 16:17:08 -0600

    commit tree e50ba020e11f117cf6744e814b3b64d371500650

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email){
+ return fetch("http://localhost:3000/users", {
+     method: 'POST',
+     headers: {
+         'Content-Type': 'application/json',
+        'Accept': 'application/json' },
+     body: JSON.stringify({name , email})
+     
+ }) 
+ .then(res=> res.json())
+ .then(obj=> {
+ const h1 = document.createElement('h1');
+ h1.textContent = obj.id;
+ document.body.appendChild(h1)
+    
+ })
+ .catch(res=> {
+    const errMsg = document.createElement("p");
+    errMsg.textContent = res.message;
+    document.body.appendChild(errMsg);
+
+ })
+}
+
+
+document.addEventListener('DOMContentLoaded', submitData)
